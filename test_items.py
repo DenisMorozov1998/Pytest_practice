495783666d03ace4222c26ba33c08214d7ab4c78
DenisMorozov1998 <morozovdenchik1998@gmail.com>
2025-07-08 15:27:57 +0300

    Update test_items.py
    Исправил все баги

--- test_items.py
+++ test_items.py
@@ -1,11 +1,13 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import time 
 
-link = 'https://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/'
+link = 'https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 def test_guest_should_see_button_add_to_basket(browser):
     browser.get(link)
-    browser.find_element(By.CSS_SELECTOR, 'button.btn-add-to-basket')
-
+    time.sleep(10)
+    add_button = browser.find_element(By.CSS_SELECTOR, 'button.btn-add-to-basket')
+    assert add_button.is_displayed(), 'Кнопки "Добавить в корзину" нет на странице' 
 
